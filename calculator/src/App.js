b86030eb07bb331a4a769e5ab78f4eed99fe1317
Valtteri Juvonen <valtteri@Valtteris-MacBook-Pro.local>
2021-03-04 12:21:24 +0100

    fixed inner workings

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,30 +1,32 @@
 import './App.css';
 import { useState } from 'react'
-import { evaluate, re } from 'mathjs'
+import { evaluate } from 'mathjs'
 
-function App() {
+export default function App() {
   const [value, setValue] = useState('0');
   const [lastValue, setLastValue] = useState('');
 
   
   const click = (event) => {
     const e = document.getElementById(event.target.id).getAttribute('value');
-    const special = ['/', '-', '+', 'X', 0];
+    const special = ['/', '-', '+', '*', '0'];
+   // const operators = ['/', '-', '+', '*'];
 
     switch(e) {
 
       case "AC":  
-        setValue('');
+        setValue('0');
         setLastValue('');
         break;
 
       case "/":
         if (lastValue != '') {
-          if (value != '/') {
-            const regex = '.*\\d';
-            setLastValue(lastValue +'/');
-            setValue('/');
-          }
+      
+          //removes excess operators from the equation
+          const match = lastValue.match('.*\\d');
+          setLastValue(match[0] +'/');
+          setValue('/');
+          
         } else {
           setLastValue(value == 0 ? '/' : value +'/');
           setValue('/');
@@ -33,13 +35,14 @@ function App() {
 
       case "X":
         if (lastValue != '') {
-          if (value != 'X') {
-            setLastValue(lastValue +'X');
-            setValue('X');
-          }
+          //removes excess operators from the equation
+          const match = lastValue.match('.*\\d');
+          setLastValue(match[0] +'*');
+          setValue('*');
+         
         } else {
-          setLastValue(value == 0 ? 'X' : value +'X');
-          setValue('X');
+          setLastValue(value == 0 ? '*' : value +'*');
+          setValue('*');
         }
         break;
 
@@ -49,88 +52,73 @@ function App() {
             setLastValue(lastValue +'-');
             setValue('-');
           }
-        } 
-        else {
-          setLastValue(value == 0 ? '-' : value +'-');
+        } else {
+          setLastValue(value == 0 ? '-' : value +'-');
           setValue('-');
         }
         break;
 
       case "+":
         if (lastValue != '') {
-          if (value != '+') {
-            setLastValue(lastValue +'+');
-            setValue('+');
-          }
-        } 
-        else {
-            setLastValue(value == 0 ? '+' : value +'+');
-            setValue('+');
+          
+          //removes excess operators from the equation
+          const match = lastValue.match('.*\\d');
+          setLastValue(match[0] +'+');
+          setValue('+');
+        } else {
+          setLastValue(value == 0 ? '+' : value +'+');
+          setValue('+');
         }
         break;
 
       case "=":
-        const result = evaluate(lastValue);
-        setLastValue(lastValue +'=' + result);
+        const result = evaluate(lastValue + value);
+        setLastValue(result.toString());
         setValue(result);
         break;
 
       case ".":
-        const valArray = value.split('');
-        setLastValue(lastValue + (valArray[valArray.length - 1] == '.' ? '' : '.'));
-        setValue(value + (valArray[valArray.length - 1] == '.' ? '' : '.'));
+        setValue(value + (value.includes('.') ? '' : '.'));
         break;
 
       case "0":
-        if (value != 0) {
-          setValue(value +'0');
-          setLastValue(lastValue +'0')
-        }
+        setValue(special.includes(value) ? '0' : value + '0');
         break;
 
       case "1":
         setValue(special.includes(value) ? '1' : value +'1');
-        setLastValue(lastValue +'1');
         break;
 
       case "2":
         setValue(special.includes(value) ? '2' : value +'2');
-        setLastValue(lastValue +'2');
         break;
 
       case "3":
         setValue(special.includes(value) ? '3' : value +'3');
-        setLastValue(lastValue +'3');
         break;   
 
       case "4":
         setValue(special.includes(value) ? '4' : value +'4');
-        setLastValue(lastValue +'4');
         break;      
 
       case "5":
         setValue(special.includes(value) ? '5' : value +'5');
-        setLastValue(lastValue +'5');
         break;          
 
       case "6":
         setValue(special.includes(value) ? '6' : value +'6');
-        setLastValue(lastValue +'6');
         break;            
 
       case "7":
         setValue(special.includes(value) ? '7' : value +'7');
-        setLastValue(lastValue +'7');
         break;             
 
       case "8":
         setValue(special.includes(value) ? '8' : value +'8');
-        setLastValue(lastValue +'8');
         break;                 
 
       case "9":
         setValue(special.includes(value) ? '9' : value +'9');
-        setLastValue(lastValue +'9');
         break;
 
       default: 
@@ -142,9 +130,8 @@ function App() {
   return (
     <div className="App">
       <div id="calculator">
-        <div id="values">
-          <h2 id="display">{value}</h2>
-          <h4 id="last-value">{lastValue}</h4>
+        <div id="display-container">
+          <h1 id="display">{value}</h1>
         </div>
         <div className="button" value="AC" id="clear" onClick={click}>AC</div>
         <div className="button" value="/" id="divide" onClick={click}>/</div>
@@ -167,5 +154,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
